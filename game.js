a543186ebaaaaca278b451522ddc199a8d05d03f
Eduardo Salermo <duduscotta@gmail.com>
2025-09-09 16:47:15 -0300

    JavaScript

--- game.js
+++ game.js
@@ -18,12 +18,19 @@ const config = {
 const game = new Phaser.Game(config);
 let player;
 let cursors;
+let enemy;
+let laser;
 
 function preload (){
-
+this.load.image('player','supermam.png');
+this.load.image('enemy','vilao.png');
+this.load.image('laser','laser.png');
 }
 function create (){
-
+player = this.physics.add.sprite(400,300,'player');
+player.setScale(0.1)
+enemy = this.physics.add.sprite(200,500,'enemy');
+enemy.setScale(0.4)
 }
 function update (){
     
