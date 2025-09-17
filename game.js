3f6126d3d708cf0a11f0e66932a698fb3a8b1802
Eduardo Salermo <duduscotta@gmail.com>
2025-09-17 19:55:41 -0300

    movimentaçao

--- game.js
+++ game.js
@@ -29,11 +29,14 @@ this.load.image('laser','laser.png');
 function create (){
 player = this.physics.add.sprite(100,500,'player');
 player.setScale(0.1)
+player.setCollideWorldBounds(true)
 enemy = this.physics.add.sprite(700,100,'enemy');
 enemy.setScale(0.4)
+enemy.setCollideWorldBounds(true)
+enemy.setVelocityY(-100)
 laser=this.physics.add.group({
     defaultKey:'laser',
-    maxSize:5,
+    maxSize:10,
     runChildUpdate:true
 })
 cursors = this.input.keyboard.createCursorKeys();
@@ -51,13 +54,22 @@ function update (){
     else if  (cursors.down.isDown){
         player.y+=3;
     }
-    if (Phaser.Input.keyboard.JustDown(cursors.space)){
+    if (Phaser.Input.Keyboard.JustDown(cursors.space)){
     playerProjetil()
+    }
+    if (enemy.y<=100){
+        enemy.setVelocityY(100)
+    }
+    else if (enemy.y>=500){
+        enemy.setVelocityY(-100)
     }
 }
 function playerProjetil(){
     const projetil=laser.get(player.x, player.y-20);
     if (projetil){
-        projetil.setActive(true).setVisible(true)
+        projetil.setActive(true).setVisible(true).setScale(0.2);
+        projetil.body.enable=true;
+        projetil.body.allowGravity=false;
+        projetil.setVelocityX(1500)
     }
 }
